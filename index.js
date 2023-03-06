419453be4b20e737a41038d33c9f3c8835d0a39f
Tahir <119082283+Tahir099@users.noreply.github.com>
2023-03-06 17:02:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const firebaseConfig = {
     appId: "1:722448566691:android:e1fd6476c55b9719167216"
   };
   
-  firebase.initializeApp(firebaseConfig);
-  
-  const storageRef = firebase.storage().ref();
-  
-  const fileInput = document.getElementById("fileInput");
-  const submitButton = document.getElementById("submitButton");
-  
-  submitButton.addEventListener("click", () => {
-    const file = fileInput.files[0];
-    const fileName = file.name;
-    const fileRef = storageRef.child(fileName);
-    fileRef.put(file).then((snapshot) => {
-      console.log("File uploaded successfully");
+
+firebase.initializeApp(firebaseConfig);
+
+const storageRef = firebase.storage().ref();
+const fileInput = document.getElementById("fileInput");
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener("click", () => {
+  const file = fileInput.files[0];
+  const fileName = file.name;
+  const fileRef = storageRef.child(fileName);
+
+  fileRef.put(file).then((snapshot) => {
+    console.log("File uploaded successfully");
+
+    // Get the download URL for the file
+    fileRef.getDownloadURL().then((url) => {
+      console.log("File URL:", url);
     });
   });
-  
+});
